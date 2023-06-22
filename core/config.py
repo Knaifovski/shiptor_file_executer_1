df1989cd6d2ec0f38e9db6d59b540faa56a026d8
NickolaQ <theknifenew@yandex.ru>
2023-06-22 18:30:44 +0300

    + update config

--- core/config.py
+++ core/config.py
@@ -1,10 +1,12 @@
 from pydantic import BaseSettings
 import os
 
-class Setting(BaseSettings):
+class Settings(BaseSettings):
+    secret_key: str
     user: str
     password: str
     shiptor_base_host: str
     shiptor_standby_base_host: str
+
     class Config:
         env_file = os.path.join(os.getcwd(),'.env')
