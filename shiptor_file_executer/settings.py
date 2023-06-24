b9c53f7f5ff2dc0f32231d5c7bebdb605d98b892
NickolaQ <theknifenew@yandex.ru>
2023-06-24 22:44:51 +0300

    + settings update

--- shiptor_file_executer/settings.py
+++ shiptor_file_executer/settings.py
@@ -131,3 +131,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Media Root
+MEDIA_ROOT = 'media/'
+MEDIA_URL = '/media/'
+
+#Files
+FILE_UPLOAD_HANDLERS = ['django.core.files.uploadhandler.TemporaryFileUploadHandler',]
